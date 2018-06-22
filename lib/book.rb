212a94889e3c65aa1a3f53f5c54e1b48d74d34f1
jessejkwk <janderson@nyatoday.com>
2018-06-22 18:22:40 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -6,3 +6,6 @@ class Books (title, author, page_count)
   end
 
 end
+
+divergent_series = Books.new ("Allegient", "Veronica Roth", "350")
+puts "My favorite book is #{title}. It's by #{author}. It's really good, and it's only #{page_count}"
