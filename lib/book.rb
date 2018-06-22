648289430d25371b75524fc43fbe0ca9086caca3
jessejkwk <janderson@nyatoday.com>
2018-06-22 18:32:43 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,8 +3,8 @@ class Books
     @title = title
     @author = author
     @page_count = page_count
+    puts "My favorite book is #{title}. It's by #{author}. It's really good, and it's only #{page_count}"
   end
-  puts "My favorite book is #{title}. It's by #{author}. It's really good, and it's only #{page_count}"
 end
 
 divergent_series = Books.new ("Allegient", "Veronica Roth", "350 pages")
